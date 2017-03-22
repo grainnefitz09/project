93367f769a85628caf8a75c0874d3bfb03349815
System Administrator <root@Grainnes-MacBook-Pro.local>
2017-03-22 13:07:37 +0000

    Buzzer code

---
+++ BuzzerCode.ino
@@ -0,0 +1,19 @@
+int buttonPin = 1; 
+int buzzerPin = 2;
+void setup()
+{
+ pinMode(buttonPin,INPUT);//set button as digital input
+ pinMode(buzzerPin,OUTPUT);//as buzzer as digital output
+}
+void loop()
+{
+ if(digitalRead(buttonPin))//check button is pressed or not
+ {
+ digitalWrite(buzzerPin,HIGH);//pressed，then buzzer buzzes
+ }
+ else
+ {
+ digitalWrite(buzzerPin, LOW);//not pressed，then buzzer remains silent
+ }
+} 
+
